e9a43c229fa42f14ba2894b2afab6c7e5de7eef1
Nikola Bijanic <nikolabijanic93@gmail.com>
2020-08-27 23:51:59 +0200

    Added RegEx pattern for recognising time-city order in sentence and thank you

--- patterns/index.js
+++ patterns/index.js
@@ -8,7 +8,7 @@ const patternDict = [
       intent: "Exit"
     },
     {
-      pattern: "\\b[za]?hva+la+n?[[\\ssa+m*]?\\sti+]?\\b",
+      pattern: "z?a*hva+la+n?\ss?a*m?(\s){0,2}t?i*",
       intent: "Thanks"
     },
     {
@@ -27,7 +27,7 @@ const patternDict = [
     },
     {
       pattern:
-        "\\b(?<time>prekosutra|sutra|danas)\\b\\svreme\\s[da]{0,2}\\s(bude|biti)\\su\\s\\b(?<city>[a-zžšćčđ]+[ a-zžšćčđ]+?)(?:\\?*)$",
+        "\\b(prekosutra|sutra|danas)\\b\\svreme[\\sda]{0,3}[\\s(bude|biti)]{0,5}\\su\\s\\b(?<city>[a-zžšćčđ]+[ a-zžšćčđ]+?)(?:\\?*)$",
       intent: "WeatherForecast"
     },
   ];
